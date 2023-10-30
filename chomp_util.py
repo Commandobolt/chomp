915c57b6fbf9c23874aec89d7bc677e7b1568f93
Commandobolt <148249343+Commandobolt@users.noreply.github.com>
2023-10-30 16:38:12 -0400

    10/30
    implemented random moments where fish can turn, also started process where user can control puffer fish

--- chomp_util.py
+++ chomp_util.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 def make_background(surface):
     #load images
@@ -36,3 +37,94 @@ def make_background(surface):
 
     for x in range(0, surface.get_width(), water.get_width()):
         surface.blit(waves, (x, waves.get_height() + 5))
+
+
+def words(background):
+    # Screen dimensions
+    scr_wid = 1200  # (px)
+    scr_hgt = 600  # (px)
+
+    # Create the screen
+    scr = pygame.display.set_mode((scr_wid, scr_hgt))
+    pygame.display.set_caption('Making a customized background')
+
+    #makes the custom word
+    custom_font = pygame.font.Font('assests/fonts/Black_Crayon.ttf', 128)
+    text = custom_font.render('Chomp', True, (255, 69, 0))
+    scr.blit(background, (0, 0))
+    scr.blit(text, (scr.get_width() / 2 - text.get_width() / 2, scr.get_height() / 2 - text.get_height() / 2))
+
+    pygame.display.flip()
+    print('SPLASH SCREEN')
+    time.sleep(1)
+    print('Running Game')
+
+
+class Fish:
+
+    def __init__(self, scr, color='green', x_spd=.2, y_spd=.1):
+        water = pygame.image.load('assests/sprites/water.png').convert()
+        sand = pygame.image.load('assests/sprites/sand.png').convert()
+        sandtop = pygame.image.load('assests/sprites/sand_top.png').convert()
+        seagrass = pygame.image.load('assests/sprites/seagrass.png').convert()
+        waves = pygame.image.load('assests/sprites/waves.png').convert()
+        fname = f'assests/sprites/{color}_fish.png'
+        self.fish_img = pygame.image.load(fname).convert()
+        self.fish_img.set_colorkey((0, 0, 0))
+        self.fish_x = random.randint(0, scr.get_width() - self.fish_img.get_width())
+        self.fish_y = random.randint(0, scr.get_height() - self.fish_img.get_height())
+        self.up = False
+        self.forward = True
+        self.fish_xspeed = scr.get_width()/(5*60)
+        self.fish_yspeed = scr.get_height()/(5*60)
+        self.fish_ybound = scr.get_height() - sand.get_height() - sandtop.get_height() - seagrass.get_height()
+        self.num_update_run = 0
+        self.num_update_run2 = random.randint(200,500)
+
+
+    def update_pos(self, scr):
+
+        #increment
+        self.num_update_run += 1
+        if (self.num_update_run >= self.num_update_run2):
+            self.up = not self.up
+            self.forward = not self.forward
+            self.fish_img = pygame.transform.flip(self.fish_img, True, False)
+            self.num_update_run2 = random.randint(200,500)
+            self.num_update_run = 0
+
+
+
+        # update fish position
+        if self.forward:
+            self.fish_x += self.fish_xspeed
+        else:
+            self.fish_x -= self.fish_xspeed
+        if self.up:
+            self.fish_y -= self.fish_yspeed
+        else:
+            self.fish_y += self.fish_yspeed
+
+
+        # check position of the fish
+        if self.fish_x > scr.get_width() - self.fish_img.get_width():
+            self.forward = False
+            self.fish_img = pygame.transform.flip(self.fish_img, True, False)
+        if self.fish_x < 0:
+            self.forward = True
+            self.fish_img = pygame.transform.flip(self.fish_img, True, False)
+        if self.fish_y > (self.fish_ybound):
+            self.up = True
+        if self.fish_y < 0:
+            self.up = False
+
+
+        # draws the fish
+        scr.blit(self.fish_img, (self.fish_x, self.fish_y))
+
+class C_Fish(Fish):
+
+    def __init__(self, screen, color):
+        super().__init__(screen, color)
+        puffer = pygame.image.load('assests/sprites/puffer_fish.png').convert()
+        self.fish_img = pygame.image.load(puffer).convert()
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 import sys
-from chomp_util import make_background
+import random
+from chomp_util import *
+import time
 
 # Initialize Pygame
 pygame.init()
+
+#Create Pygame clock
+clock = pygame.time.Clock()
 
 # Clear terminal output.
 for ii in range(0, 10):
@@ -13,7 +18,7 @@ print('\nRunning main.py.')
 print('-------------------------------------------\n')
 
 # Screen dimensions
-scr_wid = 800  # (px)
+scr_wid = 1200  # (px)
 scr_hgt = 600  # (px)
 
 # Create the screen
@@ -24,18 +29,48 @@ pygame.display.set_caption('Making a customized background')
 background = scr.copy()
 make_background(background)
 
+#Show splash screen
+words(background)
+
+#update display
+pygame.display.flip()
+
+
+bob = Fish(scr, 'orange', .4, .2)
+chad = Fish(scr, 'green')
+bo = Fish(scr, 'orange', .3, .1)
+mary = C_Fish(scr)
+
 running = True
 while running:
 
     # Get events happening in window.
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            print(f'User pressed {event.key}')
+
+        if event.type == pygame.KEYUP:
+            print(f'User pressed {event.key}')
+
+
         if event.type == pygame.QUIT:
             running = False
 
+
+
+    #draw the background
     scr.blit(background, (0,0))
 
-    # Update the display
+    #update fish position
+    bob.update_pos(scr)
+    chad.update_pos(scr)
+    bo.update_pos(scr)
+    mary.update_pos(scr)
+
+    # Update the display (sends it to the screen).
     pygame.display.flip()
+
+    clock.tick(60)
 
 # End of game loop.
 print('\n-------------------------------------------')
